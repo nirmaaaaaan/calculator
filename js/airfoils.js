4f2d8538b7e9d5c6626d1384b50621e75bbd8a0c
nirmaaaaaan <nirmanjain1604@gmail.com>
2026-06-22 12:11:23 +0530

    Update airfoils.js

--- js/airfoils.js
+++ js/airfoils.js
@@ -1,23 +1,17 @@
 const airfoils = {
-
-"S1223":{
-clmax:2.25
-},
-
-"NACA 7415":{
-clmax:1.90
-},
-
-"SD7037":{
-clmax:1.80
-},
-
-"NACA 4412":{
-clmax:1.75
-},
-
-"E423":{
-clmax:1.95
-}
-
+    "S1223": {
+        clmax: 2.25
+    },
+    "NACA 7415": {
+        clmax: 1.90
+    },
+    "SD7037": {
+        clmax: 1.80
+    },
+    "NACA 4412": {
+        clmax: 1.75
+    },
+    "E423": {
+        clmax: 1.95
+    }
 };
